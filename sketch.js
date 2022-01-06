988986c044665c98d1569115d0f38c07031a4782
nk1603 <85380205+nk1603@users.noreply.github.com>
2022-01-06 11:46:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("8552f811e95b998d9505c43a9828c6d6.jpg");
+  bg2 = loadImage("treasure.jpg")
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    fill("black")
+    textSize(40);
+    text("TREASURE UNLOCKED",250, 200);
+  }
+
+  drawSprites()
+}
